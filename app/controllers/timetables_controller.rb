5046431b13cccade6415ba3ff0f6f5312dc62939
Mary <solodovnik.my@gmail.com>
2015-11-16 19:52:12 +0200

    adds labels, new lists, new bugs

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -1,30 +1,86 @@
+require 'trello'
 require 'csv'
 require 'date'
 
 class TimetablesController < ApplicationController
 	def index
-		require 'trello'
-
-		boardName = 'Cards'
+		boardName = 'VPS'
 		username = 'sparksofl'
+		listName = 'Cist Timetable'
+		board_id = 0;
 
 		Trello.configure do |config|
 		  config.developer_public_key = 'dbaf008eabec02f406a5fdd67684780b'
 		  config.member_token = '6aed650820637b0af18adf6ffb9bc9e63f611ffd0e0a5ad0a1d3941deb728ca2'
 		end
 
+
+		@client = Trello::Client.new(
+		  :consumer_key => 'dbaf008eabec02f406a5fdd67684780b',
+		  :consumer_secret => '96e13c6bc9047aff7380b8c4f86c1696abcca8aac693f7d6428fb099bd07d81b',
+		  :oauth_token => '6aed650820637b0af18adf6ffb9bc9e63f611ffd0e0a5ad0a1d3941deb728ca2',
+		  :oauth_token_secret => '1a8250b7fa5163ae4312ff437bbe507a'
+		)
+
 		#createList = lambda { |b| Trello::List.create(name: "Cist Timetable", board_id: b.id) if b.name == 'Studies' }
 		@user = Trello::Member.find(username)
-		@board = @user.boards.each do |b| 
-			@ctList = Trello::List.create(name: "Cist Timetable", board_id: b.id) if b.name == boardName
-		end
 		
-		CSV.foreach("/home/mary/Downloads/TimeTable_15_11_2015 (1).csv", :headers => true, :encoding => "windows-1251:utf-8") do |row|
-			name = row[0].split(/[0-9]{1}/)[0]
+
+
+
+		# @label_response = @client.post("/labels", { 'name' => "testing", 'idBoard' => board_id, 'color' => "green" })
+
+		# card = Trello::Card.create(name: "sdfds", list_id: @ctList.id, card_labels: :testing)
+		# card.card_labels = [:red, '5649cf3b0d393a511ec9077a']
+		# card.update!
+		# label = @client.create("/labels", {name: 'lk', idBoard: board_id, color: :green})
+
+		list_names = Array.new 
+
+		CSV.foreach("/home/mary/Downloads/TimeTable_16_11_2015 (6).csv", :headers => true, :encoding => "windows-1251:utf-8") do |row|
+			name = row[0].split(/[0-9]{1}/)[0].gsub!(/_|\*/, '').to_s
+
+
+			type = name.split(//).last(3).join.gsub( /.{1}$/, '' ).to_s
+			if type == 'Лк'
+				c_labels = [ :yellow ]
+			elsif type == 'Лб'
+				c_labels = [ :purple ]
+			elsif type == 'Пз'
+				c_labels = [ :green ]
+			end
 			due = DateTime.strptime(row[1] + ' ' + row[2] + ' +2', '%d.%m.%Y %H:%M:%S %z')
-			card = Trello::Card.create(name: name, list_id: @ctList.id)
-			card.due = due
-			card.update!
+			#debug
+
+
+			listName = name.gsub( /.{4}$/, '' ).to_s
+
+
+			@user.boards.each do |b| 	
+				if b.name == boardName
+					if !(list_names.include? listName)
+						@ctList = Trello::List.create(name: listName, board_id: b.id)
+						if !(list_names.include? listName)
+							list_names.push(listName)
+						end
+
+
+						card = Trello::Card.create(name: name, list_id: @ctList.id, card_labels: c_labels )
+						card.due = due
+						card.update!
+					else
+						b.lists.find do |list| 
+							if list.name == listName
+								card = Trello::Card.create(name: name, list_id: list.id, card_labels: c_labels ) 
+								due = DateTime.strptime(row[1] + ' ' + row[2] + ' +2', '%d.%m.%Y %H:%M:%S %z')
+								card.due = due
+								card.update!
+							end
+						end
+					end
+				end
+				#debug
+			end
 		end
 	end
 end
